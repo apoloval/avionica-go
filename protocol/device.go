844b11c91e2c3c8280286b3d03d6c846ff32cc3c
Alvaro Polo <apoloval@gmail.com>
2017-11-27 20:38:23 +0100

    Add operations to read from a device
    Using channels, of course ;-)

--- protocol/device.go
+++ protocol/device.go
@@ -2,20 +2,35 @@ package protocol
 
 import (
 	"io"
+	"sync"
+
+	"github.com/op/go-logging"
 )
+
+const deviceReadChannelBufferSize = 64
+
+var logger = logging.MustGetLogger("avionica.device")
 
 type Device struct {
 	raw        io.ReadWriteCloser
 	portConfig PortConfig
+	handlers   messageHandlers
+	mutex      sync.Mutex
 }
 
-func NewDevice(raw io.ReadWriteCloser) Device {
-	return Device{raw, NewPortConfig()}
+func NewDevice(raw io.ReadWriteCloser) *Device {
+	device := &Device{
+		raw:        raw,
+		portConfig: NewPortConfig(),
+		handlers:   newMessageHandlers(),
+	}
+	go device.readLoop()
+	return device
 }
 
 func (dev *Device) ConfigurePorts(config PortConfig) error {
 	for rawPort := Port(portControlFirst); rawPort <= portControlLast; rawPort++ {
-		port := MustControlPort(rawPort)
+		port := ControlPort{rawPort}
 		prevBitmask := dev.portConfig.Bitmask(port)
 		newBitmask := config.Bitmask(port)
 		if prevBitmask != newBitmask {
@@ -33,4 +48,57 @@ func (dev *Device) ConfigurePorts(config PortConfig) error {
 func (dev *Device) Write(port DataPort, data MessageData) error {
 	msg := Message{Port: port.raw, Data: data}
 	return msg.encode(dev.raw)
+}
+
+func (dev *Device) AddHandler(port Port) chan MessageData {
+	dev.mutex.Lock()
+	defer dev.mutex.Unlock()
+
+	return dev.handlers.AddHandler(port)
+}
+
+func (dev *Device) Close() error {
+	return dev.raw.Close()
+}
+
+func (dev *Device) readLoop() {
+	var msg Message
+	for {
+		if err := decodeMessage(dev.raw, &msg); err != nil {
+			logger.Errorf("Failed to decode Message from device: %s", err)
+			return
+		}
+		dev.mutex.Lock()
+		dev.handlers.Handle(msg)
+		dev.mutex.Unlock()
+	}
+}
+
+type messageHandlers map[Port][]chan MessageData
+
+func newMessageHandlers() messageHandlers {
+	return make(messageHandlers)
+}
+
+func (mh messageHandlers) AddHandler(port Port) chan MessageData {
+	c := make(chan MessageData, deviceReadChannelBufferSize)
+	handlers := mh.handlersFor(port)
+	handlers = append(handlers, c)
+	mh[port] = handlers
+	return c
+}
+
+func (mh messageHandlers) Handle(msg Message) {
+	logger.Debugf("Handling read from port 0x%x, value 0x%x", msg.Port, msg.Data)
+	handlers := mh.handlersFor(msg.Port)
+	for _, handler := range handlers {
+		handler <- msg.Data
+	}
+}
+
+func (mh messageHandlers) handlersFor(port Port) []chan MessageData {
+	if handlers, found := mh[port]; found {
+		return handlers
+	}
+	return []chan MessageData{}
 }
--- protocol/device_test.go
+++ protocol/device_test.go
@@ -8,8 +8,8 @@ import (
 )
 
 func TestDevice_EnablePort(t *testing.T) {
-	buf := closeableBuffer{bytes.NewBuffer([]byte{})}
-	dev := NewDevice(buf)
+	raw := newFakeRawDevice()
+	dev := NewDevice(raw)
 	config := NewPortConfig()
 	config.Enable(MustDataPort(0))
 	config.Enable(MustDataPort(1))
@@ -17,18 +17,52 @@ func TestDevice_EnablePort(t *testing.T) {
 	err := dev.ConfigurePorts(config)
 
 	assert.NoError(t, err)
-	assert.Equal(t, []byte{0x80, 0x00, 0x03, 0x81, 0x00, 0x01}, buf.Bytes())
+	assert.Equal(t, []byte{0x80, 0x00, 0x03, 0x81, 0x00, 0x01}, raw.output.Bytes())
 }
 
 func TestDevice_Write(t *testing.T) {
-	buf := closeableBuffer{bytes.NewBuffer([]byte{})}
-	dev := NewDevice(buf)
+	raw := newFakeRawDevice()
+	dev := NewDevice(raw)
 	err := dev.Write(MustDataPort(0x42), 0x1234)
 
 	assert.NoError(t, err)
-	assert.Equal(t, []byte{0x42, 0x12, 0x34}, buf.Bytes())
+	assert.Equal(t, []byte{0x42, 0x12, 0x34}, raw.output.Bytes())
 }
 
-type closeableBuffer struct{ *bytes.Buffer }
+func TestDevice_AddHandler(t *testing.T) {
+	raw := newFakeRawDevice()
+	dev := NewDevice(raw)
+	handler := dev.AddHandler(0x42)
+	raw.receive([]byte{0x42, 0x12, 0x34})
 
-func (closeableBuffer) Close() error { return nil }
+	assert.Equal(t, MessageData(0x1234), <-handler)
+}
+
+type fakeRawDevice struct {
+	output *bytes.Buffer
+	input  chan byte
+}
+
+func newFakeRawDevice() *fakeRawDevice {
+	return &fakeRawDevice{
+		output: bytes.NewBuffer([]byte{}),
+		input:  make(chan byte),
+	}
+}
+
+func (raw *fakeRawDevice) receive(data []byte) {
+	for _, b := range data {
+		raw.input <- b
+	}
+}
+
+func (raw *fakeRawDevice) Read(p []byte) (n int, err error) {
+	p[0] = <-raw.input
+	return 1, nil
+}
+
+func (raw *fakeRawDevice) Write(p []byte) (n int, err error) {
+	return raw.output.Write(p)
+}
+
+func (fakeRawDevice) Close() error { return nil }
